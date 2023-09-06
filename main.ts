c300826d715f291f24d9c3f88ae13b9e9582e568
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-09-06 21:12:57 +0100

    created routes

--- main.ts
+++ main.ts
@@ -9,7 +9,16 @@ import "$std/dotenv/load.ts";
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import config from "./fresh.config.ts";
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+
+const envVars: DotenvConfig = await config()
 
 await start(manifest, config);
 const app = new Application();
+const router = new Router();
+
+
+router
+.get(path:"/", middleware:(context:RouterContext<"/"))
+.get()
+.post()
