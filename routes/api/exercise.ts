a9dbfbd801f5faf15b4423603240c044457042ec
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-09-13 23:22:40 +0100

    cleanup

--- routes/api/exercise.ts
+++ routes/api/exercise.ts
@@ -1,28 +1,23 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
-import Exercise from "../../models/exercise.model.js";
+import Exercise from "../../models/exercise.model";
+import exerciseController from "../../controllers/exercise.controller.ts";
+
 const router = new Router();
 
-router.route("/").get((req, res) => {
-  Exercise.find()
-    .then((exercises) => res.json(exercises))
-    .catch((err) => res.status(400).json("Error: " + err));
-});
+// router.route("/").get((req, res) => {
 
-router.route("/add").post((req, res) => {
-  const username = req.body.username;
-  const description = req.body.description;
-  const duration = Number(req.body.duration);
-  const date = Date.parse(req.body.date);
+// });
 
-  const newExercise = new Exercise({
-    username,
-    description,
-    duration,
-    date,
-  });
+// router.route("/add").post((req, res) => {
 
-  newExercise.save()
-    .then(() => res.json("Exercise added!"))
-    .catch((err) => res.status(400).json("Error: " + err));
-});
+// });
+
+// router.route("/add").post((req, res) => );
+router
+  .get("/api/exercise", exerciseController.getAllExercises)
+  .post("/api/exercise", exerciseController.createExercise)
+  .get("/api/exercise/:id", exerciseController.getExerciseById)
+  .put("/api/exercise/:id", exerciseController.updateExerciseById)
+  .delete("/api/exercise/:id", exerciseController.deleteExerciseById);
+
 export default router;
--- routes/api/user.ts
+++ routes/api/user.ts
@@ -3,21 +3,11 @@ import User from "../../models/user.model.ts";
 import userController from "../../controllers/user.controller.ts";
 const router = new Router();
 
-router.route("/").get((req, res) => {
-  User.find()
-    .then((users) => res.json(users))
-    .catch((err) => res.status(400).json("Error: " + err));
-});
+// router.route("/").get((req, res) => {
 
-router.route("/add").post((req, res) => {
-  const username = req.body.username;
+// });
 
-  const newUser = new User({ username });
-
-  newUser.save()
-    .then(() => res.json("User added!"))
-    .catch((err) => res.status(400).json("Error: " + err));
-});
+// router.route("/add").post((req, res) => );
 router
   .get("/api/user", userController.getAllUsers)
   .post("/api/user", userController.createUser)
