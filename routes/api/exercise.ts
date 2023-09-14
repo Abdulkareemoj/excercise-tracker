271575005b823bdfc2632004a5711a5e6e3f016a
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-09-14 11:50:49 +0100

    moved functions back into routes

--- routes/api/exercise.ts
+++ routes/api/exercise.ts
@@ -1,6 +1,5 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
-import Exercise from "../../models/exercise.model";
-import exerciseController from "../../controllers/exercise.controller.ts";
+import Exercise from "../../models/exercise.model.ts";
 
 const router = new Router();
 
@@ -14,10 +13,44 @@ const router = new Router();
 
 // router.route("/add").post((req, res) => );
 router
-  .get("/api/exercise", exerciseController.getAllExercises)
-  .post("/api/exercise", exerciseController.createExercise)
-  .get("/api/exercise/:id", exerciseController.getExerciseById)
-  .put("/api/exercise/:id", exerciseController.updateExerciseById)
-  .delete("/api/exercise/:id", exerciseController.deleteExerciseById);
+  .get(
+    "/api/exercise",
+    ({ request, response }: { request: any; response: any }) => {
+      Exercise.find()
+        .then((exercises) => response.json(exercises))
+        .catch((err) => response.status(400).json("Error: " + err));
+    },
+  )
+  .post("/api/exercise", (
+    { request, response }: { request: any; response: any },
+  ) => {
+    const username = request.body.username;
+    const description = request.body.description;
+    const duration = Number(request.body.duration);
+    const date = Date.parse(request.body.date);
+
+    const newExercise = new Exercise({
+      username,
+      description,
+      duration,
+      date,
+    });
+
+    newExercise.save()
+      .then(() => response.json("Exercise added!"))
+      .catch((err) => response.status(400).json("Error: " + err));
+  })
+  .get(
+    "/api/exercise/:id",
+    ({ request, response }: { request: any; response: any }) => {},
+  )
+  .put(
+    "/api/exercise/:id",
+    ({ request, response }: { request: any; response: any }) => {},
+  )
+  .delete(
+    "/api/exercise/:id",
+    ({ request, response }: { request: any; response: any }) => {},
+  );
 
 export default router;
--- routes/api/user.ts
+++ routes/api/user.ts
@@ -1,6 +1,5 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
 import User from "../../models/user.model.ts";
-import userController from "../../controllers/user.controller.ts";
 const router = new Router();
 
 // router.route("/").get((req, res) => {
@@ -9,10 +8,36 @@ const router = new Router();
 
 // router.route("/add").post((req, res) => );
 router
-  .get("/api/user", userController.getAllUsers)
-  .post("/api/user", userController.createUser)
-  .get("/api/user/:id", userController.getUserById)
-  .put("/api/user/:id", userController.updateUserById)
-  .delete("/api/user/:id", userController.deleteUserById);
+  .get(
+    "/api/user",
+    ({ request, response }: { request: any; response: any }) => {
+      User.find()
+        .then((users) => response.json(users))
+        .catch((err) => response.status(400).json("Error: " + err));
+    },
+  )
+  .post("/api/user", (
+    { request, response }: { request: any; response: any },
+  ) => {
+    const username = request.body.username;
+
+    const newUser = new User({ username });
+
+    newUser.save()
+      .then(() => response.json("User added!"))
+      .catch((err) => response.status(400).json("Error: " + err));
+  })
+  .get(
+    "/api/user/:id",
+    ({ request, response }: { request: any; response: any }) => {},
+  )
+  .put(
+    "/api/user/:id",
+    ({ request, response }: { request: any; response: any }) => {},
+  )
+  .delete(
+    "/api/user/:id",
+    ({ request, response }: { request: any; response: any }) => {},
+  );
 
 export default router;
