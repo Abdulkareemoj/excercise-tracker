0d5f40fcfe860ad88ae08f25156d721284016e37
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-09-15 22:59:53 +0100

    add routes

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,22 +4,24 @@
 
 import * as $0 from "./routes/_404.tsx";
 import * as $1 from "./routes/_app.tsx";
-import * as $2 from "./routes/api/exercise.ts";
-import * as $3 from "./routes/api/joke.ts";
-import * as $4 from "./routes/api/user.ts";
-import * as $5 from "./routes/greet/[name].tsx";
-import * as $6 from "./routes/index.tsx";
+import * as $2 from "./routes/_layout.tsx";
+import * as $3 from "./routes/api/exercise.ts";
+import * as $4 from "./routes/api/joke.ts";
+import * as $5 from "./routes/api/user.ts";
+import * as $6 from "./routes/greet/[name].tsx";
+import * as $7 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
     "./routes/_404.tsx": $0,
     "./routes/_app.tsx": $1,
-    "./routes/api/exercise.ts": $2,
-    "./routes/api/joke.ts": $3,
-    "./routes/api/user.ts": $4,
-    "./routes/greet/[name].tsx": $5,
-    "./routes/index.tsx": $6,
+    "./routes/_layout.tsx": $2,
+    "./routes/api/exercise.ts": $3,
+    "./routes/api/joke.ts": $4,
+    "./routes/api/user.ts": $5,
+    "./routes/greet/[name].tsx": $6,
+    "./routes/index.tsx": $7,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
