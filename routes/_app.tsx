e28368494cd95302d8cce3b7e7994ee740e8a518
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-09-15 23:02:04 +0100

    typo

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component }: AppProps) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>excercise-tracker</title>
+        <title>Exercise Tracker</title>
       </head>
       <body>
         <Component />
