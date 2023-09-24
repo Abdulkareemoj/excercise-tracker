280637396d2b87b15c1eb2b1dc56d36779751482
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-09-24 22:17:12 +0100

    add handlers

--- routes/create-user.tsx
+++ routes/create-user.tsx
@@ -1,53 +1,61 @@
-export default function User() {
-// constructor(props) {
-//   super(props);
-//   this.onChangeUsername = this.onChangeUsername.bind(this);
-//   this.onSubmit = this.onSubmit.bind(this);
-//   this.state = {
-//     username: ''
-//   };
-// }
+import { useState } from "preact/hooks";
+import { Handlers, PageProps } from "$fresh/server.ts";
 
+interface User {
+  username: string;
+}
+
+export const handler: Handlers<User> = {
+  async POST(req, ctx) {
+    const user = await req.json();
+    // Add user to database here
+    return new Response(JSON.stringify(user), { status: 201 });
+  },
+};
+
+export default function createUser() {
   const [username, setUsername] = useState("");
 
-    onChangeUsername = (e) => {
+  const onChangeUsername = (e: any) => {
     setUsername(e.target.value);
   };
 
-  onSubmit = (e) => {
+  const onSubmit = (e: any) => {
     e.preventDefault();
-    axios.post('http://localhost:8000/users/add', user)
-  .then(res => console.log(res.data))
-  
+    const user = {
+      username: username,
+    };
+
+    fetch("http://localhost:8000/users/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error));
+
+    setUsername("");
   };
 
-
-//   const newUser = {
-//     username: this.state.username,
-//   };
-//   console.log(newUser);
-  
-//   this.setState({
-//     username: ''
-//   })
-// }
-
-return(
-<div>
-  <h3>Create New User</h3>
-  <form onSubmit={this.onSubmit}>
-    <div className=""> 
-      <label>Username: </label>
-      <input  type="text"
-          required
-          className=""
-          value={.username}
-          onChange={onChangeUsername}
+  return (
+    <div>
+      <h3>Create New User</h3>
+      <form onSubmit={onSubmit}>
+        <div className="">
+          <label>Username:</label>
+          <input
+            type="text"
+            required
+            className=""
+            value={username}
+            onChange={onChangeUsername}
           />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
-    <div className="">
-      <input type="submit" value="Create User" className="" />
-    </div>
-  </form>
-</div>
-)}
+  );
+}
