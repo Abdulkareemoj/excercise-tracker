b7534e313da7a76258dea8bcd25a9ca9727d58f7
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2024-01-28 14:35:17 +0100

    add tailwind

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,26 +2,38 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $_404 from "./routes/_404.tsx";
-import * as $_app from "./routes/_app.tsx";
-import * as $api_joke from "./routes/api/joke.ts";
-import * as $greet_name_ from "./routes/greet/[name].tsx";
-import * as $index from "./routes/index.tsx";
-import * as $Counter from "./islands/Counter.tsx";
-import { type Manifest } from "$fresh/server.ts";
+import * as $_404 from './routes/_404.tsx';
+import * as $_app from './routes/_app.tsx';
+import * as $_layout from './routes/_layout.tsx';
+import * as $api_exercise from './routes/api/exercise.ts';
+import * as $api_joke from './routes/api/joke.ts';
+import * as $api_user from './routes/api/user.ts';
+import * as $create_exercise from './routes/create-exercise.tsx';
+import * as $create_user from './routes/create-user.tsx';
+import * as $exerciselist from './routes/exerciselist.tsx';
+import * as $greet_name_ from './routes/greet/[name].tsx';
+import * as $index from './routes/index.tsx';
+import * as $Counter from './islands/Counter.tsx';
+import { type Manifest } from '$fresh/server.ts';
 
 const manifest = {
-  routes: {
-    "./routes/_404.tsx": $_404,
-    "./routes/_app.tsx": $_app,
-    "./routes/api/joke.ts": $api_joke,
-    "./routes/greet/[name].tsx": $greet_name_,
-    "./routes/index.tsx": $index,
-  },
-  islands: {
-    "./islands/Counter.tsx": $Counter,
-  },
-  baseUrl: import.meta.url,
+	routes: {
+		'./routes/_404.tsx': $_404,
+		'./routes/_app.tsx': $_app,
+		'./routes/_layout.tsx': $_layout,
+		'./routes/api/exercise.ts': $api_exercise,
+		'./routes/api/joke.ts': $api_joke,
+		'./routes/api/user.ts': $api_user,
+		'./routes/create-exercise.tsx': $create_exercise,
+		'./routes/create-user.tsx': $create_user,
+		'./routes/exerciselist.tsx': $exerciselist,
+		'./routes/greet/[name].tsx': $greet_name_,
+		'./routes/index.tsx': $index,
+	},
+	islands: {
+		'./islands/Counter.tsx': $Counter,
+	},
+	baseUrl: import.meta.url,
 } satisfies Manifest;
 
 export default manifest;
